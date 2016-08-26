d345b393960d1af420546e82cbc41040bbd8e37d
wxj <wangxj@jiudeng.me>
2016-08-26 12:49:02 +0800

    init

---
+++ wheel/src/main/java/kankan/wheel/widget/model/CityModel.java
@@ -0,0 +1,22 @@
+package kankan.wheel.widget.model;
+
+import java.util.List;
+
+/**
+ * Created by jiudeng007 on 2016/1/4.
+ */
+public class CityModel {
+    /**
+     * 城市id
+     */
+    public int Id;
+    /**
+     * 市名称
+     */
+    public String Name;
+    /**
+     * 地区信息，数据为数组类型
+     */
+    public List<DataModel> Districts;
+
+}
---
+++ wheel/src/main/java/kankan/wheel/widget/model/DataModel.java
@@ -0,0 +1,14 @@
+package kankan.wheel.widget.model;
+
+/**
+ * Created by jiudeng009 on 2016/4/12.
+ */
+public class DataModel {
+    public int Id;
+    public String Name;
+
+    public DataModel(int id, String name) {
+        this.Id = id;
+        this.Name = name;
+    }
+}
